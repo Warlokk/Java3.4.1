3e6ed2e38db77730bcbf5b3a719f8e14f56bf775
Yuriy Nikolaev <warlokk83@gmail.com>
2021-02-19 20:10:52 +0400

    Methods added, migrated to repository

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -4,30 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.netology.domain.MovieItem;
+import ru.netology.repository.AfishaRepository;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class AfishaManager {
-    private MovieItem[] items = new MovieItem[0];
+    private AfishaRepository repository;
     private int feedCount = 10;
 
+    public AfishaManager(AfishaRepository repository) {
+        this.repository = repository;
+    }
+
     public AfishaManager(int feedCount) {
-            this.feedCount = feedCount;
+        this.feedCount = feedCount;
     }
 
     public void add(MovieItem item) {
-        int length = items.length + 1;
-        MovieItem[] tmp = new MovieItem[length];
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
+        repository.save(item);
     }
 
     public MovieItem[] getMoviesForFeed() {
+        MovieItem[] items = repository.findAll();
         if (this.feedCount > items.length)
-                 feedCount = items.length;
+            feedCount = items.length;
         MovieItem[] result = new MovieItem[feedCount];
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
@@ -37,12 +38,26 @@ public class AfishaManager {
     }
 
     public MovieItem[] getAll() {
+        MovieItem[] items = repository.findAll();
         MovieItem[] result = new MovieItem[items.length];
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+
+    public MovieItem findById(int id) {
+        MovieItem movie = repository.findById(id);
+        return movie;
+    }
+
+    public void removeAll() {
+        repository.removeAll();
     }
 
 }
